20cdc5870cdc11e617a7810111af9cc387ee0dfc
Tobias McNulty <tobias@caktusgroup.com>
2014-10-10 14:53:15 -0400

    fix netrc usage; flake8 cleanup

--- run-tests.py
+++ run-tests.py
@@ -4,13 +4,11 @@ import datetime
 import shutil
 import time
 import os
-import sys
 import requests
 
 from contextlib import contextmanager
 from github import Github
-from subprocess import check_call, check_output
-from fabric.api import *
+from subprocess import call, check_call, check_output
 from fabulaws.ubuntu.instances.base import UbuntuInstance
 
 for l in ['fabulaws', 'fabric']:
@@ -23,17 +21,19 @@ github_password = os.environ['GITHUB_PASSWORD']
 
 
 @contextmanager
-def github_cmd_line_auth(user, passwd):
+def github_cmd_line_auth(user, passwd, home):
     restore_netrc = False
-    if os.path.exists('.netrc'):
+    netrc = os.path.join(home, '.netrc')
+    netrcbak = os.path.join(home, '.netrc.bak')
+    if os.path.exists(netrc):
         restore_netrc = True
-        os.path.rename('.netrc', '.netrc.bak')
-    with open('.netrc', 'w') as f:
-        f.write('machine github.com\n  login {0}\n  password {1}'.format(user, passwd))
+        os.rename(netrc, netrcbak)
+    with open(netrc, 'w') as f:
+        f.write('machine github.com\nlogin {0}\npassword {1}\n'.format(user, passwd))
     yield
-    os.remove('.netrc')
+    os.remove(netrc)
     if restore_netrc:
-        os.path.rename('.netrc', '.netrc.bak')
+        os.path.rename(netrc, netrcbak)
 
 
 class TestServer(UbuntuInstance):
@@ -54,6 +54,7 @@ class TestServer(UbuntuInstance):
         )
         self.setup()
 
+
 def bootstrap():
     name = datetime.datetime.now().strftime('dpt_test_%Y_%m_%d_%H_%M_%S')
     venv_name = 'env_{}'.format(name)
@@ -68,7 +69,7 @@ def bootstrap():
         g = Github(github_user, github_password)
         u = g.get_user()
         gh_repo = u.create_repo(name)
-        check_call(['django-admin.py', 'startproject', 
+        check_call(['django-admin.py', 'startproject',
                     '--template=https://github.com/caktus/django-project-template'
                     '/zipball/dpt-test-support',
                     '--extension=py,rst', name])
@@ -97,12 +98,12 @@ def bootstrap():
                     gh_repo.clone_url])
         env = os.environ.copy()
         env['HOME'] = os.path.abspath(os.path.dirname(proj_path))
-        with github_cmd_line_auth(github_user, github_password):
+        with github_cmd_line_auth(github_user, github_password, env['HOME']):
             check_call(['git', 'push', '-u', 'origin', 'master'], env=env)
         dev_reqs = os.path.join('requirements', 'dev.txt')
         venv('pip', ['install', '-q', '-r', dev_reqs])
         fab(['setup_master', '-H', serv_name])
-        fab(['sync:1']) # force overwrite of server secrets
+        fab(['sync:1'])  # force overwrite of server secrets
         fab(['--set', 'environment=master', 'setup_minion:salt-master', '-H', serv_name])
         fab(['staging', 'setup_minion:web,balancer,db-master,cache,queue,worker', '-H', serv_name])
         sleep_time = 10
@@ -122,13 +123,14 @@ def bootstrap():
             if r.status_code == 200:
                 print 'got 200 status code (attempt={})!'.format(i)
                 break
-            print 'got bad status code ({}). attempt={}, content={}'.format(r.status_code, i, r.content)
+            print 'got bad status code ({}). attempt={}, content={}'.format(r.status_code,
+                                                                            i, r.content)
     finally:
         try:
             gh_repo.delete()
         except NameError:
             pass
-        local('rm -rf {} {}'.format(proj_path, venv_path))
+        call(['rm', '-rf', proj_path, venv_path])
 
 
 if __name__ == "__main__":
